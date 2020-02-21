8f3345ea1471216ce5310f27e4c042da849eba25
Tisdale Fry <tisdalefry@gmail.com>
2020-02-21 04:14:45 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -16,7 +16,7 @@ def introduction_with_language(name, language)
   "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language(name, language)
+introduction_with_language
 
 language = "Ruby"
 def introduction_with_language_optional(name, language)
